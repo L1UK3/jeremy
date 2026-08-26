8caf0dafbb1291906eae001855af05a19a1083be
L1UK3 <lukeenness@outlook.com>
2026-08-26 15:55:18 +0100

    now debugs the runtime directory rather than the submssion

--- simulation/episode.py
+++ simulation/episode.py
@@ -93,7 +93,7 @@ class Episode:
 
 
 if __name__ == "__main__":
-    episode = Episode(agent1=".out/submission.py", agent2="starter", debug=True)
+    episode = Episode(agent1="src/main.py", agent2="starter", debug=True)
     result = episode.run()
 
     final_inventory = (
