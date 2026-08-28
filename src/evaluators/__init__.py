b2f43815309a230a8cbc62e32d244ad296888a46
L1UK3 <lukeenness@outlook.com>
2026-08-28 19:10:13 +0100

    refactor(evaluators): consolidate evaluators into single module with SRP step helpers

--- src/evaluators/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from evaluators.expansion import evaluate_expansion
-from evaluators.livestock import evaluate_livestock
-from evaluators.market import evaluate_market
-
-__all__ = ["evaluate_expansion", "evaluate_livestock", "evaluate_market"]
